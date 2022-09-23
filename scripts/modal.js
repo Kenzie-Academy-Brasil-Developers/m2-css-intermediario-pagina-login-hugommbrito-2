ca66b76a1fccfb8fc2f4d13481723d41cd76fd3d
Hugo Brito <hugomdmb@gmail.com>
2022-09-22 23:26:26 -0300

    Primeiro Commit da Entrega

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,9 @@
-/* Desenvolva seu código aqui... */
+let botoesModal = document.querySelectorAll("[data-control-modal]")
+
+for (let i = 0; i < botoesModal.length; i++) {
+    botoesModal[i].addEventListener('click', function(){
+        let modalId = botoesModal[i].getAttribute("data-control-modal")
+        document.getElementById(modalId).classList.toggle('hideModal')
+        console.log("peguei o clique")
+    })
+}
